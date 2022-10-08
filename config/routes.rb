333fd10c4111dbfcb5f90b3883197a2148214f3c
Narumi Abe <smile.narukkory@gmail.com>
2022-10-08 04:38:37 +0000

    [Add]ユーザーに関する機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users,skip: [:passwords]
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-
   resources :videos
+  resources :users, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
